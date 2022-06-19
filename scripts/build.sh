2159f32d0d82ddf54fb3ef25d01a91006a716632
yinqiwen <yinqiwen@gmail.com>
2022-06-19 18:25:41 +0800

    update build shell

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,11 +8,22 @@ case "${unameOut}" in
     MINGW*)     machine=MinGw;;
     *)          machine="UNKNOWN:${unameOut}"
 esac
-echo ${machine}
 
+platform="linux_musl_x64"
+if [ "$#" -ge 1 ]; then
+    arch=$1
+    case "${arch}" in
+        x64)         platform="linux_musl_x64";;
+        arm_eabi)    platform="linux_musl_arm_eabi";;
+        arm_eabihf)  platform="linux_musl_arm_eabihf";;
+        *)           platform="linux_musl_x64";; 
+esac
+fi
+
+echo ${machine} ${platform}
 
 if [ "$machine" = "Linux" ]; then
-    bazel build --define local_musl=true --features=fully_static_link --cxxopt=-std=c++20 --incompatible_enable_cc_toolchain_resolution --platforms=//toolchains:linux_musl_x64 //...
+    bazel build --define local_musl=true --features=fully_static_link --cxxopt=-std=c++20 --incompatible_enable_cc_toolchain_resolution --platforms=//toolchains:"${platform}" //...
 else
     if [[ $(uname -m) == 'arm64' ]]; then
         bazel build --cxxopt=-std=c++20  //...
